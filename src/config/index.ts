0918f35aa50b0e9b28edbf84a8aa1a23c9f5ff84
abedshamieh-dev <abdulrahmanshamieh234@gmail.com>
2026-05-02 16:35:20 +0300

    Prepare app for Railway deploy

--- src/config/index.ts
+++ src/config/index.ts
@@ -10,7 +10,8 @@ export default {
     isDev: process.env.NodeEnv === "development",
     isProduction: process.env.NodeEnv === "production",
     port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
-    host: process.env.HOST || "localhost",
+    // Railway requires binding to all interfaces.
+    host: process.env.HOST || "0.0.0.0",
     dbMode: DBMode.SQLITE,
     auth: {
         secretKey: process.env.JWT_SECRET_KEY || "secret",
